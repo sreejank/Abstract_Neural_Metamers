89d2f06118a40dfa9457392e27759c1852b1006d
Sreejan Kumar <sreejank@tigercpu.princeton.edu>
2022-04-01 14:45:00 -0400

    Modify metamer code notes

--- metamer_distribution_generator.py
+++ metamer_distribution_generator.py
@@ -1,6 +1,6 @@
 """
-Code to generate metamer task distributions from a given abstraction (supply name with command line argument)
-"""
+Code to generate metamer task distributions from a given abstraction (supply name with command line argument). This will save a .pt file in data/ that the enviornments will interface with. 
+""" 
 import torch
 import torch.nn.functional as F
 import numpy as np   
@@ -18,40 +18,15 @@ class Net(torch.nn.Module):
 		self.fc1 = torch.nn.Linear(S*S, S*S)
 		self.fc2 = torch.nn.Linear(S*S, S*S)
 		self.fc3 = torch.nn.Linear(S*S, S*S)
-		#self.fc4 = torch.nn.Linear(S*S, S*S)
-		#self.fc5 = torch.nn.Linear(S*S, S*S)
 
 	def forward(self,x):
 		x = F.relu(self.fc1(x))
 		x = F.relu(self.fc2(x))
-		#x = F.relu(self.fc3(x))
-		#x = F.relu(self.fc4(x))
-		#x = self.fc5(x)
 		x=self.fc3(x) 
 
 		return F.sigmoid(x)
 
 S=7 
-"""
-class Net(torch.nn.Module):
-	def __init__(self,S=7):
-		super(Net,self).__init__()
-		self.conv1=torch.nn.Conv2d(1,16,3,stride=1)
-		self.conv2=torch.nn.Conv2d(16,16,3,stride=1)
-		self.fc1=torch.nn.Linear(144,72)
-		self.fc2=torch.nn.Linear(72,49)
-		self.fc3=torch.nn.Linear(49,49)
-	def forward(self,x):
-		x=torch.reshape(x,(-1,1,S,S))
-		x=F.relu(self.conv1(x))
-		x=F.relu(self.conv2(x))
-		#print(x.shape)
-		x=torch.reshape(x,(-1,144))
-		x=F.relu(self.fc1(x))
-		x=F.relu(self.fc2(x))
-		x=self.fc3(x)
-		return F.sigmoid(x)
-"""
 
 network=Net() 
 #network.load_state_dict(torch.load("data/perceptrons_task_distribution_"+name+".pt"))   
